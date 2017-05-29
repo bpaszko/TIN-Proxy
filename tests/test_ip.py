1ec3caa23e774e916a4d63fe78af3e9c42ba5e69
Marcin <marcinnsuwala@gmail.com>
2017-05-29 17:17:07 +0200

    Increment test cases

--- tests/test_ip.py
+++ tests/test_ip.py
@@ -7,7 +7,7 @@ s_start_port = 20000
 register = 2000
 value = 120
 
-for i in range(10):
+for i in range(100):
 	command = ' '.join([python, path, str(register + i), str(value + 1), str(start_port + i)])
 	command2 = ' '.join([python, path2, str(register + i), str(value + 1), str(s_start_port + i)])
 	os.system(command)	
